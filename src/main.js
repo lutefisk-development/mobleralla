97eb894536b254925e5f74c5d6e1fe41d1e7e4e2
Per Kristian Svanberg <kristiansvanberg@gmail.comgit config --global user.name lutefisk-development>
2019-01-10 09:50:20 +0100

    added the project to the repo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/src/jquery.js'
+import 'popper.js/dist/popper.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
