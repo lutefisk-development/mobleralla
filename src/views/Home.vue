c052eae0aac4fa51ba1206d00f4f0bea32d2516c
Aaron Ahmadi <aaron.k.ahmadi@gmail.com>
2019-01-11 12:43:59 +0100

    fixade bugg som gjorde att sidan inte laddade

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,22 @@
 <template>
   <div class="home">
-<<<<<<< Updated upstream
     <Navbar></Navbar>
     <h1>DETTA ÄR STARTSIDAN</h1>
-=======
     <homeContent></homeContent>
->>>>>>> Stashed changes
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-<<<<<<< Updated upstream
 import Navbar from '@/components/Navbar.vue'
 
-=======
 import homeContent from '@/components/home-content.vue'
 import HelloWorld from '@/components/HelloWorld.vue'
->>>>>>> Stashed changes
 export default {
   name: 'home', 
   components: {
-<<<<<<< Updated upstream
-    Navbar
-=======
+    Navbar,
     homeContent
->>>>>>> Stashed changes
   }
 }
 </script>
