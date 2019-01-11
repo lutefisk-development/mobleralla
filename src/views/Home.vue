75280e1008361c571c965aee3001cabcc8b64daf
Aaron Ahmadi <aaron.k.ahmadi@gmail.com>
2019-01-11 12:33:12 +0100

    Ändrat i home, bland annat css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
+<<<<<<< Updated upstream
     <Navbar></Navbar>
     <h1>DETTA ÄR STARTSIDAN</h1>
+=======
+    <homeContent></homeContent>
+>>>>>>> Stashed changes
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+<<<<<<< Updated upstream
 import Navbar from '@/components/Navbar.vue'
 
+=======
+import homeContent from '@/components/home-content.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+>>>>>>> Stashed changes
 export default {
   name: 'home', 
   components: {
+<<<<<<< Updated upstream
     Navbar
+=======
+    homeContent
+>>>>>>> Stashed changes
   }
 }
 </script>
+
+
+<!--<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+-->
+
